7fe3151addaf18194f02d5fb5ff7fef9e7a2b87d
Jim Doyle <indellibleink@gmail.com>
2020-04-27 21:32:53 -0400

    add check boxes and radio buttons

--- SpringMVC/WebContent/WEB-INF/view/student-confirmation.jsp
+++ SpringMVC/WebContent/WEB-INF/view/student-confirmation.jsp
@@ -1,11 +1,21 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Student Confirmation Form</title>
 </head>
 <body>
-	<p>The Student is confirmed: ${student.firstName} ${student.lastName}</p>
-	<br>
+	The Student is confirmed: ${student.firstName} ${student.lastName}
+	<br> 
 	Country: ${student.country}
+	<br>
+	Favorite Language: ${student.favoriteLanguage}
+	<br>
+	Operating Systems:
+	<ul>
+		<c:forEach var="temp" items="${student.operatingSystems}">
+		<li>${temp}</li>
+		</c:forEach>
+	</ul>
 </body>
 </html>
--- SpringMVC/src/example/Student.java
+++ SpringMVC/src/example/Student.java
@@ -8,6 +8,9 @@ public class Student {
 	private String lastName;
 	private String country;
 	private LinkedHashMap<String, String> countryOptions;
+	private String favoriteLanguage;
+	private String[] operatingSystems;
+	// private LinkedHashMap<Integer, String> selectedLanguage;
 
 	public Student() {
 
@@ -17,6 +20,9 @@ public class Student {
 		countryOptions.put("US", "United States");
 		countryOptions.put("UK", "United Kingdom");
 		countryOptions.put("DE", "Germany");
+		
+		// populate Map with key number and language
+		
 	}
 
 	public String getFirstName() {
@@ -46,5 +52,22 @@ public class Student {
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
 	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+	
 
 }
