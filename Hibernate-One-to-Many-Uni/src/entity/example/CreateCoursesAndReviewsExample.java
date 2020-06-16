85d65b43adf58888462e021feebfad81f082e1ab
Jim Doyle <indellibleink@gmail.com>
2020-06-16 19:09:26 -0400

    Working on One to Many Bidirectional

---
+++ Hibernate-One-to-Many-Uni/src/entity/example/CreateCoursesAndReviewsExample.java
@@ -0,0 +1,53 @@
+package entity.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import entity.Course;
+import entity.Instructor;
+import entity.InstructorDetail;
+import entity.Review;
+
+public class CreateCoursesAndReviewsExample {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure().addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).addAnnotatedClass(Review.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+			// get the instructor from db
+			int instructorId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, instructorId);
+			
+			// create some courses
+			Course tempCourse1=new Course ("Computer Science Principles");
+			Course tempCourse2=new Course ("Algorithms");
+			
+			// add courses to instructor
+			tempInstructor.add(tempCourse1);
+			tempInstructor.add(tempCourse2);
+			
+			// save the courses
+			session.save(tempCourse1);
+			session.save(tempCourse2);
+
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+}
